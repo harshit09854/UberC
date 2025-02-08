a3f77548482394acf2e6fb17d1e438f3db64d4fc
Harshit pokharna <Harshitpokharna24@gmail.com>
2025-02-08 10:16:18 +0530

    first commit

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -55,3 +55,9 @@ module.exports.loginUser = async (req, res, next) => {
 module.exports.getUserProfile = async (req, res, next) => {
   res.status(200).json(req.user);
 };
+
+
+module.exports.getUserProfile = async (req, res, next) => {
+ res.status(200).json(req.user);
+
+}
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require("express-validator")
 const userController = require('../controllers/user.controller');
-
+const authMiddleware = require('../middleware/auth_middleware');  
 router.post(
   "/register",
   [
@@ -27,5 +27,7 @@ router.post(
   ],
   userController.loginUser
 );
+
+router.get("/profile",authMiddleware.authUser, userController.getUserProfile);
 
 module.exports = router;
