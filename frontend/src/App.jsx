6f8b92cdd59ff91a140b9e3fbffef031ab1b87fb
Harshit pokharna <Harshitpokharna24@gmail.com>
2025-02-22 18:52:49 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,52 @@
-import React, { useContext } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Start from './pages/Start'
-import UserLogin from './pages/UserLogin'
-import UserSignup from './pages/UserSignup'
-import CaptainSignup from './pages/CaptainSignup' 
-import CaptainLogin from './pages/CaptainLogin'
-import Userlogout from './pages/Userlogout' 
-import Home from './pages/Home'
-import UserProtectWrapper from './pages/UserProtectWrapper'
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import Start from "./pages/Start";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainSignup from "./pages/CaptainSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+import Userlogout from "./pages/Userlogout";
+import Home from "./pages/Home";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
+import CaptainHome from "./pages/CaptainHome";
+import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
 const App = () => {
-
   return (
     <div>
       <Routes>
-         <Route path='/' element={<Start/>} />
-         <Route path='/login' element={<UserLogin/>} />
-         <Route path='/signup' element={<UserSignup/>} />
-         <Route path='/CaptainLogin' element={<CaptainLogin/>} />
-         <Route path='/captain-signup' element={<CaptainSignup/>} />
-        <Route path='/home' element={
-          <UserProtectWrapper>
-            <Home />
-          </UserProtectWrapper>
-        } />
-<Route path='user/logout' element={<UserProtectWrapper>
-    <Userlogout/>
-  </UserProtectWrapper>
-  } />
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/CaptainLogin" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
 
-
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="user/logout"
+          element={
+            <UserProtectWrapper>
+              <Userlogout />
+            </UserProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain-home"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainHome />
+            </CaptainProtectWrapper>
+          }
+        />
       </Routes>
-       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      home
+    <div className='h-screen relative'>
+      <img className='w-16 absolute left-5 top-5' src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png" alt="" />
+
+      <div className='h-screen w-screen'>
+        <img className='h-fulll w-full object-cover' src="https://images.squarespace-cdn.com/content/v1/54ff63f0e4b0bafce6932642/1613585187225-CJE61PHC7ZYVEXYYVWBK/Lyft%27s+Map+vs+Uber%27s+Map.png" alt="" />
+      </div>
+      <div className=' flex flex-col jusitfy-end  h-screen absolute top-0 w-full'>
+        <div className='h-[30%] p-5 bg-white'>
+        <h4 className='text-2xl font-semibold'>Find a trip</h4>
+        <form >
+          <input className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5' type="text" placeholder='Add a pick-up location' />
+        <input className='bg-[#eee] px-12 py-2 text-base rounded-lg mt-3 w-full' type="text" placeholder='Enter your destination' />
+        </form>
+        </div>
+        <div className='  bg-red-500 h-0'>
+
+        </div>
+      </div>
     </div>
   )
 }
